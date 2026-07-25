bb8e8f66a0b0269366371aef3d3c24863b29469e
tajernigan <tajernigan@mines.edu>
2026-07-25 15:48:33 -0400

    implemented scrambling cube with wait time

--- src/cube.js
+++ src/cube.js
@@ -1,10 +1,35 @@
 import "cubing/twisty";
 
-const cube = document.getElementById("middle-cube")
+const SCRAMBLE_COUNT = 20;
+let scrambled = false;
+const moves = ["R", "U", "F", "L", "D", "B"];
+const directions = ["'", "", "2"];
+const cube = document.getElementById("middle-cube");
+
+const sleep = milliseconds =>
+    new Promise(resolve => setTimeout(resolve, milliseconds));
+
+const random_move = () => {
+    return moves[Math.floor(Math.random() * moves.length)] + directions[Math.floor(Math.random() * directions.length)]; // get random move type and direction
+}
 
 const scramble_button = document.getElementById("scramble")
 scramble_button.onclick = async () => {
-    cube.experimentalAddMove("R");
+    if (scrambled === true) return;
+    scrambled = true;
+    let scramble = [];
+    
+    for (let i = 0; i < SCRAMBLE_COUNT; i++){
+        const move = random_move();
+        scramble.push(move);
+        await sleep(500);
+        cube.experimentalAddMove(move);
+    }
+    
+    scrambled = false;
+
+    const scramble_string = scramble.join(" ");
+    console.log(scramble_string);
     const response = await fetch(
         "/api/message",
         {
@@ -12,7 +37,7 @@ scramble_button.onclick = async () => {
         headers: {
             'Content-Type': 'text/plain'
         },
-        body: "someone just hit scramble"
+        body: scramble_string
     });
 
     const message = await response.text();
@@ -20,7 +45,6 @@ scramble_button.onclick = async () => {
 }
 
 window.addEventListener("keydown", (event) => {
-    const moves = ["R", "U", "F", "L", "D", "B"];
     const key_pressed = event.key
     if (moves.includes(key_pressed.toUpperCase())) {
         const move = key_pressed === key_pressed.toUpperCase() ? key_pressed : key_pressed.toUpperCase() + "'";
